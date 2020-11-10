c0a9a728093df2ac099548942d363ae6e73643c9
djib2011 <thanostagaris@gmail.com>
2020-11-10 16:51:18 +0200

    Added data with 24 windows in new format.

--- orchistrator.py
+++
@@ -1,4 +0,0 @@
-python datasets/augment_data.py -i 18 -n 23000 --no_window
-python datasets/augment_data.py -i 18 -n 230000 --no_window
-python datasets/augment_data.py -i 18 -n 2300000 --no_window
-
