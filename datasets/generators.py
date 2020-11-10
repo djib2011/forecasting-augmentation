7d56c4563628d19254ecf6e252aa21f66327cb99
djib2011 <thanostagaris@gmail.com>
2020-11-10 16:47:43 +0200

    Fixed bug in generator.

--- datasets/generators.py
+++ datasets/generators.py
@@ -1,21 +1,21 @@
 import numpy as np
 import tensorflow as tf
+import h5py
 
 
-def seq2seq_generator(x_path: str, y_path: str, batch_size: int = 256, shuffle: bool = True) -> tf.data.Dataset:
+def seq2seq_generator(data_path: str, batch_size: int = 256, shuffle: bool = True) -> tf.data.Dataset:
     """
     Factory for building TensorFlow data generators for loading time series data.
     Also supports data augmentation and loading series with overlap for backcast.
 
-    :param x_path: Path of a csv file that contains X
-    :param y_path: Path of a csv file that contains y
+    :param data_path: Path of a HDF5 file that contains X and y
     :param batch_size: The batch size
     :param shuffle: True/False whether or not the data will be shuffled.
     :return: A TensorFlow data generator.
     """
 
     # Load data
-    with h5py.File(n, 'r') as hf:
+    with h5py.File(data_path, 'r') as hf:
         x = np.array(hf.get('X'))
         y = np.array(hf.get('y'))
 
@@ -28,8 +28,6 @@ def seq2seq_generator(x_path: str, y_path: str, batch_size: int = 256, shuffle: 
         data = data.shuffle(buffer_size=len(x))
     data = data.repeat()
     data = data.batch(batch_size=batch_size)
-    if augmentation:
-        data = data.map(augment)
     data = data.prefetch(buffer_size=1)
 
     data.__class__ = type(data.__class__.__name__, (data.__class__,), {'__len__': lambda self: len(x)})
