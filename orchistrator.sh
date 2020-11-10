ae8a9c175659e4850c514299801ef91b78e78136
djib2011 <thanostagaris@gmail.com>
2020-11-10 16:49:11 +0200

    Updated orchistration.

---
+++ orchistrator.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python training/combinations_samples_nw.py -n 23000
+python training/combinations_samples_nw.py -n 230000 
+python training/combinations_samples_nw.py -n 2300000 
+python training/combinations_samples_nw.py -n 23000000
+
