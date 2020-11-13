3b6e39357ac067b7939a6c6d630dfa4664cedb42
djib2011 <thanostagaris@gmail.com>
2020-11-13 13:19:44 +0200

    Added function for loading test set.

--- datasets/utils.py
+++ datasets/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 from typing import Union
 
 
@@ -16,3 +17,17 @@ def get_last_N(series: Union[pd.Series, np.ndarray], N: int = 18):
         pad = [ser_N[0]] * (N - len(ser_N))
         ser_N = np.r_[pad, ser_N]
     return ser_N
+
+
+def load_test_set(data_dir: Union[Path, str] = 'data'):
+
+    train_path = Path(data_dir) / 'Yearly-train.csv'
+    test_path = Path(data_dir) / 'Yearly-test.csv'
+
+    train_set = pd.read_csv(train_path).drop('V1', axis=1)
+    test_set = pd.read_csv(test_path).drop('V1', axis=1)
+
+    X_test = np.array([get_last_N(ser[1], N=18) for ser in train_set.iterrows()])
+    y_test = test_set.values
+
+    return X_test, y_test
