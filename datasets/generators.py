3ec7c5392909f5fa7c08cfb4fe6e6dbf0e47041c
djib2011 <thanostagaris@gmail.com>
2020-12-04 12:42:46 +0200

    Added generator for artemis data.

--- datasets/generators.py
+++ datasets/generators.py
@@ -34,30 +34,52 @@ def seq2seq_generator(data_path: str, batch_size: int = 256, shuffle: bool = Tru
     return data
 
 
+def artemis_generator(data_path, batch_size=256, data_col_start=4, shuffle=True):
+    data = np.load(data_path, allow_pickle=True)
+
+    x = data[:, data_col_start:-6]
+    y = data[:, -6:]
+
+    x = x[np.all((y < 10) & (y > -10), axis=1)]
+    y = y[np.all((y < 10) & (y > -10), axis=1)]
+
+    gen = tf.data.Dataset.from_tensor_slices((x, y))
+    if shuffle:
+        gen = gen.shuffle(buffer_size=len(x))
+    gen = gen.repeat()
+    gen = gen.batch(batch_size=batch_size)
+    gen = gen.prefetch(buffer_size=1)
+
+    gen.__class__ = type(gen.__class__.__name__, (gen.__class__,), {'__len__': lambda self: len(x)})
+
+    return gen
+
+
 if __name__ == '__main__':
 
-    import argparse
-
+    # import argparse
+    #
     # Parse command line arguments
-    parser = argparse.ArgumentParser()
+    # parser = argparse.ArgumentParser()
+    #
+    # parser.add_argument('-i', '--input_len', type=int, default=12, help='Insample length.')
+    # parser.add_argument('-c', '--combinations', type=int, default=2, help='Number of series combined to produce augmentations.')
+    # parser.add_argument('-n', '--num_samples', type=int, default=23000, help='Number of augmented samples.')
+    # parser.add_argument('--no_window', action='store_true', help='Don\'t generate windows from the timeseries.')
+    #
+    # args = parser.parse_args()
+    #
+    # window = args.input_len + 6
+    #
+    # if args.no_window:
+    #     x_path = 'data/yearly_{}_aug_by_{}_num_{}_nw.csv'.format(window, args.combinations, args.num_samples)
+    #     y_path = 'data/yearly_{}_y_nw.csv'.format(window)
+    # else:
+    #     x_path = 'data/yearly_{}_aug_by_{}_num_{}.csv'.format(window, args.combinations, args.num_samples)
+    #     y_path = 'data/yearly_{}_nw.csv'.format(window)
 
-    parser.add_argument('-i', '--input_len', type=int, default=12, help='Insample length.')
-    parser.add_argument('-c', '--combinations', type=int, default=2, help='Number of series combined to produce augmentations.')
-    parser.add_argument('-n', '--num_samples', type=int, default=23000, help='Number of augmented samples.')
-    parser.add_argument('--no_window', action='store_true', help='Don\'t generate windows from the timeseries.')
-
-    args = parser.parse_args()
-
-    window = args.input_len + 6
-
-    if args.no_window:
-        x_path = 'data/yearly_{}_aug_by_{}_num_{}_nw.csv'.format(window, args.combinations, args.num_samples)
-        y_path = 'data/yearly_{}_y_nw.csv'.format(window)
-    else:
-        x_path = 'data/yearly_{}_aug_by_{}_num_{}.csv'.format(window, args.combinations, args.num_samples)
-        y_path = 'data/yearly_{}_nw.csv'.format(window)
-
-    gen = seq2seq_generator(x_path, y_path, batch_size=256, shuffle=True)
+    data_path = '/home/artemis/AugmExp/Data/train_in18_all_windows.npy'
+    gen = artemis_generator(data_path, batch_size=256, shuffle=True)
 
     for x, y in gen:
         print('Train set:')
