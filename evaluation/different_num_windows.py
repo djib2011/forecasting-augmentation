a0a27e95b34143277cc45370d1249e6f888f0f29
djib2011 <thanostagaris@gmail.com>
2020-12-15 12:03:52 +0200

    Added evaluation script for windows experiment.

---
+++ evaluation/different_num_windows.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.append('.')
+
+import evaluation
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--debug', action='store_true',
+                        help='Run in debug mode: Don\'t evaluate any of the models; print lots of diagnostic messages.')
+
+    args = parser.parse_args()
+
+    result_dir = 'results/windows/'
+    report_dir = 'reports/windows/'
+
+    columns = ['input_len', 'num_samples']
+
+    evaluation.run_evaluation(result_dir=result_dir, report_dir=report_dir, columns=columns, debug=args.debug,
+                              snapshot=False, exclude_pattern='113k')
