3125ed194c3f12494b3b9c9e03c7e558f8407db0
djib2011 <thanostagaris@gmail.com>
2020-12-18 14:15:54 +0200

    Furthered experiment and results on optimization hyperparameters.

--- evaluation/training_hyperparameters.py
+++ evaluation/training_hyperparameters.py
@@ -20,4 +20,5 @@ if __name__ == '__main__':
 
     columns = ['optimizer', 'learning_rate', 'amsgrad', 'exponential_decay']
 
-    evaluation.run_evaluation(result_dir=result_dir, report_dir=report_dir, columns=columns, debug=args.debug, snapshot=False)
+    evaluation.run_evaluation(result_dir=result_dir, report_dir=report_dir, columns=columns,
+                              debug=args.debug, snapshot=False, exclude_pattern='rms')
