b5fa034f3f0f919bc8fe0d8d8d265e5f4953d9c3
djib2011 <thanostagaris@gmail.com>
2020-12-18 17:25:51 +0200

    Added documentation for common training functions.

---
+++ training/architecture_investigation.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+if os.getcwd().endswith('training'):
+    os.chdir('..')
+sys.path.append(os.getcwd())
+
+import tensorflow as tf
+import datasets
+import models
+import os
+import argparse
+
+import training
+
+# Global configs
+num_runs = 4
+batch_size = 1024
+epochs = 15
+snapshot = False
+warmup = 0
+patience = 1
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i', '--input_len', type=int, default=18, help='Insample length.')
+parser.add_argument('--debug', action='store_true', help='Run in debug mode: Don\'t train any of the models and print '
+                                                         'lots of diagnostic messages.')
+
+args = parser.parse_args()
+
+data_path = 'data/yearly_{}.h5'.format(args.input_len + 6)
+
+data = datasets.seq2seq_generator(data_path, batch_size=1024)
+
+hp_comb_dict = {'base_layer_size': [128, 256],
+                'direction': ['bi'],
+                'depth': [2],
+                'input_seq_length': [18],
+                'output_seq_length': [6],
+                'property': ['small_dense', 'dropout', 'batchnorm', 'layernorm']}
+
+hp_generator = training.make_runs(hp_comb_dict)
+
+for hp in hp_generator:
+
+    model_gen = models.get_experimental(family='sequential', type=hp['direction'],
+                                        depth=hp['depth'], property=hp['property'])
+
+    run_name = 'architectures/dir_{}__size_{}__depth_{}__property_{}'.format(hp['input_seq_length'],
+                                                                             hp['direction'],
+                                                                             hp['base_layer_size'],
+                                                                             hp['depth'],
+                                                                             hp['property'])
+    if args.debug:
+        print('run name:', run_name)
+
+    training.run_training(model_gen, hp, data, run_name, num_runs=num_runs, debug=args.debug, batch_size=batch_size,
+                          epochs=epochs, snapshot=snapshot, warmup=warmup, patience=patience)
--- training/common.py
+++ training/common.py
@@ -4,9 +4,21 @@ import itertools
 import utils
 import os
 import sys
+from typing import Union
 
+def train_model_snapshot(model: tf.keras.models.Model, train_set: tf.data.Dataset, run_name: str,
+                         run_num: Union[str, int], cycles: int = 15, batch_size: int = 256) -> tf.keras.models.Model:
+    """
+    Train a model using Snapshot Ensembles with SGDA.
 
-def train_model_snapshot(model, train_set, run_name, run_num, cycles=15, batch_size=256):
+    :param model: An instance of a keras model
+    :param train_set: A generator containing the training data
+    :param run_name: Name of the current run
+    :param run_num: Number of the current run (used to indicate multiple cold restarts)
+    :param cycles: Number of cycles (i.e. how many times to store weights)
+    :param batch_size: The batch size
+    :return: The same instance of the model as the first argument
+    """
     result_dir = 'results/'
 
     epochs = cycles + 5
@@ -19,7 +31,25 @@ def train_model_snapshot(model, train_set, run_name, run_num, cycles=15, batch_s
     return model
 
 
-def train_model_single(model, train_set, run_name, run_num, epochs=15, batch_size=256, exp_decay=False, **kwargs):
+def train_model_single(model: tf.keras.models.Model, train_set: tf.data.Dataset, run_name: str,
+                       run_num: Union[str, int], epochs: int = 15, batch_size: int = 256, exp_decay: bool = True,
+                       **kwargs) -> tf.keras.models.Model:
+    """
+    Trains a single keras model.
+
+    :param model: An instance of a keras model
+    :param train_set: A generator containing the training data
+    :param run_name: Name of the current run
+    :param run_num: Number of the current run (used to indicate multiple cold restarts)
+    :param epochs: Number of epochs
+    :param batch_size: The batch size
+    :param exp_decay: Indication to use an exponential decay of the learning rate
+    :param kwargs: Arguments to be passed to the checkpoint callback
+           - warmup: how many epochs to wait before starting to save weights
+           - patiance: every how many epochs to start storing weights
+           - verbose: print every time weights are stored (doesn't work properly)
+    :return: The same instance of the model as the first argument
+    """
 
     steps_per_epoch = len(train_set) // batch_size + 1
     result_file = 'results/{}__{}/'.format(run_name, run_num) + 'weights_epoch_{epoch:03d}.h5'  # TODO: dynamic :03d
@@ -37,7 +67,26 @@ def train_model_single(model, train_set, run_name, run_num, epochs=15, batch_siz
     return model
 
 
-def run_training(model_gen, hparams, data, run_name, num_runs=5, debug=False, snapshot=False, exp_decay=False, **kwargs):
+def run_training(model_gen, hparams: dict, data: tf.data.Dataset, run_name: str, num_runs: int = 5, debug: bool = False,
+                 snapshot: bool = False, **kwargs) -> None:
+    """
+    Function that handles the training of multiple models for use in ensembles.
+
+    :param model_gen: Factory for producing model instances according to hparams
+    :param hparams: A dictionary containing the hyperparameters used for creating the model instance
+    :param data: A generator containing the training data
+    :param run_name:  Number of the current run (used to indicate multiple cold restarts)
+    :param num_runs: Number of epochs
+    :param debug: Debug mode. Don't actually run training and store weights, but see if it works properly.
+    :param snapshot: Indicate the use of snapshot ensembles or not.
+    :param kwargs: Various training arguments, including:
+           - exp_decay: Indication to use an exponential decay of the learning rate
+           - epochs: Number of epochs for each training
+           - batch_size: The batch size
+           - warmup: how many epochs to wait before starting to save weights
+           - patiance: every how many epochs to start storing weights
+           - verbose: print every time weights are stored (doesn't work properly)
+    """
 
     single_model_training_fcn = train_model_snapshot if snapshot else train_model_single
 
@@ -50,7 +99,7 @@ def run_training(model_gen, hparams, data, run_name, num_runs=5, debug=False, sn
     else:
         for i in range(num_runs):
             model = model_gen(hparams)
-            _ = single_model_training_fcn(model, data, run_name, run_num=i, exp_decay=exp_decay, **kwargs)
+            _ = single_model_training_fcn(model, data, run_name, run_num=i, **kwargs)
             del model
             tf.keras.backend.clear_session()
 
