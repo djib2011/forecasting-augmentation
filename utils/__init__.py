b923cb899419eb2cc3e22f6d49a21a416745ef7b
djib2011 <thanostagaris@gmail.com>
2020-12-28 17:24:14 +0200

    Added decorator for printing deprecation warnings.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,13 @@
+import warnings
+import functools
 from utils import callbacks, metrics, optimizers
+
+
+def deprecated(func):
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        warnings.warn('Function "{}" is deprecated!'.format(func.__name__))
+        return func(*args, **kwargs)
+
+    return inner
