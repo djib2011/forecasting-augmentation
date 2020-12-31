24d991b6a09a2dce64e1029b3b70153a8ff19a9c
djib2011 <thanostagaris@gmail.com>
2020-12-31 18:37:50 +0200

    Orchistrator update.

--- orchistrator.sh
+++ orchistrator.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-python training/combinations_samples_nw.py -n 14000000 -c 2
-python training/combinations_samples_nw.py -n 14000000 -c 3
-#python training/combinations_samples_nw.py -n 14000000 -c 4
-#python training/combinations_samples_nw.py -n 14000000 -c 50
+python evaluation/architecture_investigation.py
+python evaluation/different_num_windows.py
 
+#python training/different_num_windows.py -n all_windows
+#python training/different_num_windows.py -n 113k
+#python training/different_num_windows.py -n 56k
+#python training/different_num_windows.py -n 28k
