d6a2bc4778df497885f0ccb1a259595f170d0dce
djib2011 <thanostagaris@gmail.com>
2021-01-05 12:37:56 +0200

    Add function for retrieving best model setup

--- models/__init__.py
+++ models/__init__.py
@@ -11,3 +11,12 @@ def get_experimental(family, type, depth, property):
 
     if family == 'sequential':
         return sequential_exp.model_names['{}_{}_{}'.format(type, depth, property)]
+
+
+def get_optimal_setup(hparams):
+
+    hparams['input_seq_length'] = 18
+    hparams['output_seq_length'] = 6
+    hparams['size'] = 128
+
+    return sequential_exp.model_names['bi_3_batchnorm'], hparams
